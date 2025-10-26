20c9cdf35ca5de853ff94accf5253fcf3a40c870
MarriccZwei <marricc7@gmail.com>
2025-10-26 15:45:05 -0700

    last minute changes

--- CPEN-321-M1-app/frontend/app/src/main/java/com/cpen321/usermanagement/ui/components/BackActionButton.kt
+++ CPEN-321-M1-app/frontend/app/src/main/java/com/cpen321/usermanagement/ui/components/BackActionButton.kt
@@ -1,15 +1,67 @@
 package com.cpen321.usermanagement.ui.components
 
 import Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.cpen321.usermanagement.R
 import com.cpen321.usermanagement.ui.theme.LocalSpacing
 
+//import Icon
+//import androidx.compose.foundation.layout.size
+//import androidx.compose.material3.IconButton
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import com.cpen321.usermanagement.R
+//import com.cpen321.usermanagement.ui.theme.LocalSpacing
+//
+//@Composable
+//fun BackActionButton(
+//    onClick: () -> Unit,
+//    modifier: Modifier = Modifier
+//) {
+//    val spacing = LocalSpacing.current
+//
+//    IconButton(
+//        onClick = onClick,
+//        modifier = modifier.size(spacing.extraLarge2)
+//    ) {
+//        BackIcon()
+//    }
+//}
+//@Composable
+//private fun BackIcon() {
+//    Icon(
+//        name = R.drawable.ic_arrow_back,
+//    )
+//}
+
 @Composable
 fun BackActionButton(
+    onClick: ()->Unit,
+    modifier: Modifier = Modifier
+){
+    BottomAppBar(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Absolute.Left,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ContentNoteActionButton(onClick = onClick)
+        }
+    }
+}
+
+@Composable
+private fun ContentNoteActionButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -19,11 +71,12 @@ fun BackActionButton(
         onClick = onClick,
         modifier = modifier.size(spacing.extraLarge2)
     ) {
-        BackIcon()
+        ContentIcon()
     }
 }
+
 @Composable
-private fun BackIcon() {
+private fun ContentIcon() {
     Icon(
         name = R.drawable.ic_arrow_back,
     )
