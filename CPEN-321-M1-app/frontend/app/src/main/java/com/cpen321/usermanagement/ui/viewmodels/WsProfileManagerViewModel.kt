656848d19c40f6f11de82152b08f3a3da59e3f9d
MarriccZwei <marricc7@gmail.com>
2025-11-03 15:02:58 -0800

    Update WsProfileManagerViewModel.kt
    another label for codacy

--- CPEN-321-M1-app/frontend/app/src/main/java/com/cpen321/usermanagement/ui/viewmodels/WsProfileManagerViewModel.kt
+++ CPEN-321-M1-app/frontend/app/src/main/java/com/cpen321/usermanagement/ui/viewmodels/WsProfileManagerViewModel.kt
@@ -102,8 +102,8 @@ class WsProfileManagerViewModel@Inject constructor(
                 workspaceProfilePicture = pictureUri.toString(),
                 workspaceId = navigationStateManager.getWorkspaceId()
             )
-            if (result.isSuccess) {
-                val currentWorkspace = _uiState.value.workspace ?: return@launch
+            if (result.isSuccess and (_uiState.value.workspace != null)) {
+                val currentWorkspace = _uiState.value.workspace!!
                 val updatedWorkspace = currentWorkspace.copy(
                     profile = currentWorkspace.profile.copy(imagePath = pictureUri.toString())
                 )
